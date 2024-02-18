ba02c0b91708432b7beee5ec447cf4e11e48cdd3
diasha003 <fursevichdiana42@gmail.com>
2024-02-19 00:01:30 +0300

    change envFilePath

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { AuthModule } from './auth/auth.module';
     TickersModule,
     PrismaModule,
     ConfigModule.forRoot({
-      envFilePath: `${process.cwd()}\\config\\${process.env.NODE_ENV}.env`,
+      envFilePath: `${process.cwd()}/config/${process.env.NODE_ENV}.env`,
       isGlobal: true,
     }),
   ],
